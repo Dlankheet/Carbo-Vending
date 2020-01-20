56f58c205738954850909718fac1c11c7ec6fb06
Marlon <marlon_scholten@live.nl>
2020-01-20 13:46:57 +0100

    links naar voorbeeldfiles en onnodige product files verwijderd

--- EngineAPI-master/app.py
+++ EngineAPI-master/app.py
@@ -1,14 +1,39 @@
-from flask import Flask, send_from_directory, jsonify, redirect, request
+from flask import Flask, send_from_directory, jsonify, redirect, request, render_template
+# from machinefuncties import *
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Jordan Peterson',
+        'title': '12 Rules for life',
+        'content': 'eye opening book',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'George Orwell',
+        'title': '1984',
+        'content': 'very scary book',
+        'date_posted': 'April 28, 2018'
+    }
+]
+
 @app.route('/')
+@app.route('/home')
 def home():
-    return redirect("/index.html", code=302)
+    return render_template('home.html', posts=posts)
+
+@app.route('/heidelberglaan')
+def heidelberglaan():
+    return render_template('heidelberglaan.html')
+
+@app.route('/padualaan')
+def padualaan():
+    return render_template('padualaan.html')
 
 @app.route('/<path:filename>')
 def download_file(filename):
     return send_from_directory('static', filename, as_attachment=False)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
