b28232662fcd6d2b2b2bee8bc3e8293416c09d4b
Marlon <marlon_scholten@live.nl>
2020-01-23 13:34:02 +0100

    styling en onderhoud balkje toegevoegd

---
+++ EngineAPI-master/voorspelling-test.py
@@ -0,0 +1,13 @@
+import datetime
+
+today = datetime.today()
+# records = get_data()
+# first_record_date = min(records[‘date’])
+# startdate = today - first_record_date
+# timeframe-data = []
+
+print(today)
+
+# for record in records:
+#     if record[‘date’] in range(startdate, today):
+#         timeframe-data.append(record)
